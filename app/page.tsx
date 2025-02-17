e3edb8fc54a1cf629a57c9aff7d55bdbbeb29764
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-17 18:16:35 -0500

    bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import Grid from "@/components/grid";
 import Hero from "@/components/hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navItems } from "@/data";
+import { nav } from "motion/react-client";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
---
+++ components/grid.tsx
@@ -0,0 +1,15 @@
+import { FaCss3, FaJs, FaReact } from "react-icons/fa6"
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid"
+import { gridItems } from "@/data"
+
+const Grid = () => {
+  return (
+    <section id="#about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map(item => (<BentoGridItem key={item.id} {...item} />))}
+      </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
